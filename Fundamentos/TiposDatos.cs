c84245f97bb6678d1dbc1905fc78b29592be9eba
Ariel Paricagua <arielfparicaguav@gmail.com>
2025-10-29 16:09:19 -0400

    fix: Correcciones de configuracion

--- Fundamentos/TiposDatos.cs
+++ Fundamentos/TiposDatos.cs
@@ -1,11 +1,9 @@
 namespace MiProyectoCSharp.Fundamentos;
-
 public class TiposDatos
 {
     public static void Ejecutar()
     {
         Console.WriteLine("\n--- Tipos de Datos ---");
-        
         // Tipos numéricos
         byte edad = 25;              // 0 a 255
         int habitantes = 1000000;    // -2,147,483,648 a 2,147,483,647
@@ -13,14 +11,11 @@ public class TiposDatos
         float precio = 19.99f;       // Decimal de precisión simple
         double distancia = 384400.5; // Decimal de doble precisión
         decimal dinero = 1234.56m;   // Alta precisión para dinero
-        
         // Tipos de texto
         char inicial = 'M';
         string mensaje = "Hola C#";
-        
         // Tipos lógicos
         bool activo = true;
-        
         Console.WriteLine($"byte: {edad}");
         Console.WriteLine($"int: {habitantes}");
         Console.WriteLine($"long: {poblacion}");
--- obj/Debug/net9.0/MiProyectoCSharp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MiProyectoCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiProyectoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d51ad1f8ded3da0718614221e224ac280eba0882")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44d5a63a572034332ec7dee5eeaa5c920d5becf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiProyectoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiProyectoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
